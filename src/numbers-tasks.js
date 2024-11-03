2eebf54efee24a97cc839367025b2dfb5a47ef30
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-03 15:49:22 +0400

    feat: add getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -291,8 +291,12 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((acc, curr) => {
+      return acc + +curr;
+    }, 0);
 }
 
 /**
