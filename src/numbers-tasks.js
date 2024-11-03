45fb637c146d25ac082fe04a10ee11a3b6c75d08
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-03 11:04:24 +0400

    feat: add getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,8 +105,14 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarProduct = x1 * x2 + y1 * y2;
+  const lengthFirstVector = Math.sqrt(x1 ** 2) + Math.sqrt(y1 ** 2);
+  const lengthSecondVector = Math.sqrt(x2 ** 2) + Math.sqrt(y2 ** 2);
+  const result = Math.acos(
+    scalarProduct / (lengthFirstVector * lengthSecondVector)
+  );
+  return result;
 }
 
 /**
