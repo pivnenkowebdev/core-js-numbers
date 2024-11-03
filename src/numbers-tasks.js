67ca57f3e68c2e75335ed264efa0575b31c0387a
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-03 15:45:30 +0400

    add: feat getSumTon

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -272,8 +272,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let result = n;
+  for (let i = 1; i < n; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 /**
