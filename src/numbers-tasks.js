e390cf4e16d587be1531e64e4b5b1276d9b8adcc
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-04 08:43:21 +0400

    feat: add isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -202,8 +202,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i * i <= n; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
